48bfd5ddc81bec9346f9aa77f44f7044aaf2d3d3
Ruben Martinez-Cantin <rmcantin@unizar.es>
2011-05-31 22:45:35 +0200

    Added random generator for candidate problem

--- mcmc.py
+++ mcmc.py
@@ -14,15 +14,21 @@
 #
 
 
-from pylab import *
-from numpy import *
-from numpy.random import *
+import pylab as pl
+import numpy as np
+from numpy.random import rand, randn
 
 
 # Target functions
 #=================
-def fgaussian(x, sigma): return exp(-0.5 * (x)**2 / sigma**2)/(sqrt(2*pi)*sigma)
-def fmixture(x,sigma): return 0.3*fgaussian(x,sigma) + 0.7*fgaussian(x-10,sigma)
+def fgaussian(x, sigma):
+     return np.exp(-0.5 * (x)**2 / sigma**2)/(np.sqrt(2*np.pi)*sigma)
+
+def fmixture(x,sigma):
+     return 0.3*fgaussian(x,sigma) + 0.7*fgaussian(x-10,sigma)
+
+def sample_proposal(sigma):
+     return sigma * randn(1)
 
 
 # DEFINITIONS:
@@ -32,7 +38,7 @@ SA = False                #  If no annealing, the algorithm is exactly MH
 
 N = 5000                  #  Number of iterations.
 sigma = 2                 #  Standard deviation of the target components.
-x = zeros((N,1))          #  Markov chain (unknowns).
+x = np.zeros((N,1))       #  Markov chain (unknowns).
 sigma_prop = 10           #  Standard deviation of the Gaussian proposal.
 N_bins = 50               #  Number of bins in the histogram.
 
@@ -46,14 +52,14 @@ x[0,0] = 20*rand(1)       #  Initial point of the Markov Chain.
 
 for i in range(1,N):
      u = rand(1)
-     z = sigma_prop * randn(1)
+     z = sample_proposal(sigma_prop)
      alpha1 = fmixture(x[i-1]+z,sigma)  # Probability of the sample after the move
      alpha2 = fmixture(x[i-1],sigma)    # Probability of the sample before the move
      alpha = (alpha1/alpha2)
 
      if SA:
 	alpha = alpha**(1/T_i)          # Use annealing if required.
-	T_i = T_i*CS                    # Adjust the cooling schedule. (only used for annealing)
+	T_i = T_i*CS                    # Adjust the cooling schedule.
 
      if (u < alpha):
         x[i] = x[i-1] + z               # Accept jump
@@ -66,13 +72,14 @@ print x
 # PLOT THE HISTOGRAMS:
 # ====================
 def plot_all( spIndex , mcIndex ):
-     subplot(spIndex)
-     x_t = linspace(-10,20,1000)
-     n, bins, patches = hist(x[0:mcIndex], N_bins, normed=1, facecolor='green', alpha=0.75)
-     plot(x_t,fmixture(x_t,sigma),'k',linewidth=2)
-     axis([-10, 20, 0, 0.15])
+
+     pl.subplot(spIndex)
+     x_t = np.linspace(-10,20,1000)
+     n, bins, patches = pl.hist(x[0:mcIndex], N_bins, normed=1, facecolor='green', alpha=0.75)
+     pl.plot(x_t,fmixture(x_t,sigma),'k',linewidth=2)
+     pl.axis([-10, 20, 0, 0.15])
      plot_name = 'Iteration = %d' % mcIndex
-     title(plot_name)
+     pl.title(plot_name)
 
 
 plot_all(221,100)
@@ -80,4 +87,4 @@ plot_all(222,500)
 plot_all(223,1000)
 plot_all(224,N)
 
-show()
+pl.show()
---
+++ randomCandidates.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import unicodedata
+import random
+
+dnihash = "TRWAGMYFPDXBNJZSQVHLCKE"
+
+# For troubles with Spanish names
+def strip_accents(string):
+  return unicodedata.normalize('NFKD', unicode(string, 'ISO-8859-1')).encode('ASCII', 'ignore')
+
+
+def getRandomDNI():
+    num = random.randint(10000000,99999999)
+    letra = dnihash[num%23]
+    return str(num)+letra
+
+def getRandomName(lines):
+    name = random.choice(lines)
+    name = strip_accents(name)
+    return name.rstrip().title()
+
+with open('nombres.txt','r') as fnom:
+    with open('apellidos.txt','r') as fapp:
+        with open('candid.txt','w') as fcan:
+            nombres  = fnom.readlines()
+            apellidos = fapp.readlines()
+            for i in range(1,100):
+                fecha = str(random.randint(1,28))+";"+str(random.randint(1,12))+";"+str(random.randint(1950,1985))
+                nombrecompleto = getRandomName(apellidos)+";"+getRandomName(apellidos)+";"+getRandomName(nombres)
+                nota = str(random.randint(5,9))+"."+str(random.randint(0,9))
+                linea = getRandomDNI()+";"+nombrecompleto+";"+fecha+";"+nota
+                print linea
+                fcan.write(linea+"\n")
+
