bd5f0be1e3cc259812f536f4420c4c76b28cbf1f
Ruben Martinez-Cantin <rmcantin@unizar.es>
2011-06-14 13:03:39 +0200

    Solving enconding problem and adding command line options to Moodle parser

--- parsernames.py
+++ parsernames.py
@@ -5,26 +5,23 @@
 # Author : Ruben Martinez-Cantin
 # Date   : 03-26-2011
 
-
+import optparse
 import re
 import sys
 import os
 import unicodedata
 
-inputfile = 'fundinfo.htm'
-urlmoodle = 'https:\/\/moodle\.unizar\.es'
-outputfile = 'rename_all'
-
 # For troubles with Spanish names
-def strip_accents(string):
-  return unicodedata.normalize('NFKD', unicode(string, 'ISO-8859-1')).encode('ASCII', 'ignore')
+def strip_accents(string,encoding):
+  return unicodedata.normalize('NFKD', unicode(string, encoding)).encode('ASCII', 'ignore')
 
-def cleanupName(name):
-    name = strip_accents(name)
+def cleanupName(name,encoding):
+    name = strip_accents(name,encoding)
     name = name.title()
     return name.replace(' ','')
 
-with open(inputfile,'r') as f:
+def generatefiles(inputfile,encoding,urlmoodle,outputfile):
+  with open(inputfile,'r') as f:
     # print source
     source = f.read()
     values = re.findall('<strong><a href="' + urlmoodle +
@@ -33,24 +30,35 @@ with open(inputfile,'r') as f:
                         '(.*?)<\/a><\/strong>',
                         source)
     if os.name == 'posix':
-        # Linux or Mac OS
-        outputfile = outputfile + '.sh'
-        header = '#!/bin/sh \n'
-        mvcommand = 'mv'
+      # Linux or Mac OS
+      outputfile = outputfile + '.sh'
+      header = '#!/bin/sh \n'
+      mvcommand = 'mv'
     elif os.name == 'nt':
-        # Windows
-        outputfile = outputfile + '.bat'
-        header = ''
-        mvcommand = 'move'
+      # Windows
+      outputfile = outputfile + '.bat'
+      header = ''
+      mvcommand = 'move'
     else:
-        sys.exit("Operating system not supported")
+      sys.exit("Operating system not supported")
         
-    with open(outputfile,'w') as fw:
-        fw.write(header)
+  with open(outputfile,'w') as fw:
+    fw.write(header)
                 
-        for value in values:
-          name = cleanupName(value[1])
-          fw.write(mvcommand + ' ' + value[0] + ' ')
-          fw.write(name + '\n')
-          print name, 'corresponds to ID =', value[0]
- 
+    for value in values:
+      name = cleanupName(value[1],encoding)
+      fw.write(mvcommand + ' ' + value[0] + ' ')
+      fw.write(name + '\n')
+      print name, 'corresponds to ID =', value[0]
+
+def run():
+  parser = optparse.OptionParser()
+  parser.add_option('-i', '--inputfile', default = 'fundinfo.htm')
+  parser.add_option('-e', '--encoding', default = 'utf-8')
+  parser.add_option('-u', '--url', default = 'https:\/\/moodle\.unizar\.es')
+  parser.add_option('-o', '--outputfile', default = 'rename_all')
+  (options, args) = parser.parse_args()
+  generatefiles(options.inputfile,options.encoding,options.url,options.outputfile)
+
+if __name__ == "__main__":
+  run()
